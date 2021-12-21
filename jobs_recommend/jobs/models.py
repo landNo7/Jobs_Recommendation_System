e9186ed712be458d9c0f52cc821fe5e22bc5e4e8
landno7 <349798632@qq.com>
2021-12-21 20:12:55 +0800

    create mysql table model

--- jobs_recommend/jobs/models.py
+++ jobs_recommend/jobs/models.py
@@ -20,3 +20,60 @@ class JobsData(models.Model):
     class Meta:
         managed = False
         db_table = 'jobs_data'
+
+
+class Table1User(models.Model):
+    user_id = models.CharField(primary_key=True, max_length=255)
+    live_city_id = models.CharField(max_length=255, blank=True, null=True)
+    desire_jd_city_id = models.CharField(max_length=255, blank=True, null=True)
+    desire_jd_industry_id = models.CharField(max_length=255, blank=True, null=True)
+    desire_jd_type_id = models.CharField(max_length=255, blank=True, null=True)
+    desire_jd_salary_id = models.CharField(max_length=255, blank=True, null=True)
+    cur_industry_id = models.CharField(max_length=255, blank=True, null=True)
+    cur_jd_type = models.CharField(max_length=255, blank=True, null=True)
+    cur_salary_id = models.CharField(max_length=255, blank=True, null=True)
+    cur_degree_id = models.CharField(max_length=255, blank=True, null=True)
+    birthday = models.CharField(max_length=255, blank=True, null=True)
+    start_work_date = models.CharField(max_length=255, blank=True, null=True)
+    experience = models.TextField(db_collation='utf16_general_ci', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'table1_user'
+
+
+class Table2Jd(models.Model):
+    jd_no = models.CharField(primary_key=True, max_length=255)
+    jd_title = models.CharField(max_length=255, blank=True, null=True)
+    company_name = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    jd_sub_type = models.CharField(max_length=255, blank=True, null=True)
+    require_nums = models.CharField(max_length=255, blank=True, null=True)
+    max_salary = models.CharField(max_length=255, blank=True, null=True)
+    min_salary = models.CharField(max_length=255, blank=True, null=True)
+    start_date = models.CharField(max_length=255, blank=True, null=True)
+    end_date = models.CharField(max_length=255, blank=True, null=True)
+    is_travel = models.CharField(max_length=255, blank=True, null=True)
+    min_years = models.CharField(max_length=255, blank=True, null=True)
+    key = models.CharField(max_length=255, blank=True, null=True)
+    min_edu_level = models.CharField(max_length=255, blank=True, null=True)
+    max_edu_level = models.CharField(max_length=255, blank=True, null=True)
+    is_mangerial = models.CharField(max_length=255, blank=True, null=True)
+    resume_language_required = models.CharField(max_length=255, blank=True, null=True)
+    job_description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'table2_jd'
+
+
+class Table3Action(models.Model):
+    user_id = models.CharField(max_length=255, blank=True, null=True)
+    jd_no = models.CharField(max_length=255, blank=True, null=True)
+    browsed = models.CharField(max_length=255, blank=True, null=True)
+    delivered = models.CharField(max_length=255, blank=True, null=True)
+    satisfied = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'table3_action'
